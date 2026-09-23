8b5d3972df41df7ed006556637e377213544ef89
Anna <mysina93@mail.ru>
2026-09-23 15:32:33 +0300

    Framework: Set

--- src/org/skypro/skyshop/product/SimpleProduct.java
+++ src/org/skypro/skyshop/product/SimpleProduct.java
@@ -5,7 +5,7 @@ public class SimpleProduct extends Product {
 
     public SimpleProduct(String name, int price) {
         super(name);
-        if (price<=0){
+        if (price <= 0) {
             throw new IllegalArgumentException("Цена продукта должна быть строго больше 0.");
         }
         this.price = price;
@@ -20,9 +20,10 @@ public class SimpleProduct extends Product {
     public boolean isSpecial() {
         return false;
     }
+
     @Override
-    public String toString(){
-        return getName()+": "+getPrice();
+    public String toString() {
+        return getName() + ": " + getPrice();
     }
 }
 
